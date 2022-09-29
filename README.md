1d284abe6c3ffd02bec66d8cf4fb809682eae901
Mehtab Riar <mehtabriar228@hotmail.com>
2022-09-28 20:37:35 -0700

    finished

--- README.md
+++ README.md
@@ -25,3 +25,38 @@
 ## Credit and Collaborations
 
 [Web Response cycle](https://excalidraw.com/#json=YinBAi29xWgbKGbyiqwaz,kAybwK-ipBJsaxwMNzhRYg)
+
+## City explorer api
+
+**Author**: Mehtab Riar
+**Version**: 1.1.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview2
+
+- In this project we linked our backend server to two different API's so it could retrieve data from each one depending on the searchQuery. We then had our frontend site retrieve that data with the keyword 'get'
+
+## Getting Started2
+
+- You don't need to install anything new. For some reason I was asked to redownload axios but outside of that no new programs were downloaded
+
+## Architecture2
+
+- Our backend server uses the keyword 'get' to request data from the two API's we added and then we had our frontend site use the key word 'get' to retrieve data from our backend server. I built seperate components to display the data on react. I still need to style them better.
+
+## Change Log2
+
+09-28-2022 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. We added two new API's to retrieve data from
+
+## Credit and Collaborations2
+
+Name of feature: 2 new API's
+
+Estimate of time needed to complete: 3 hours
+
+Start time: 1pm
+
+Finish time: 7pm
+
+Actual time needed to complete: 6 hours
+
+[Web Response cycle2](https://excalidraw.com/#json=vId87eKW4kvNJxkYOdxQE,PfFVOCNfRsvrumMo4NJmjQ)
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const cors = require('cors');
 // load data
 const data = require('./data/data.json')
+const axios = require('axios');
 // start our server
 const app = express();
 // Middleware
@@ -16,12 +17,7 @@ const PORT = process.env.PORT || 3001;
 //listening for connection
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
 
-class WeatherForecast {
-    constructor(date, h, l) {
-        this.description = `Low of ${l}, high of ${h} with broken clouds`
-        this.date = date;
-    }
-}
+
 
 //endpoints:
 
@@ -29,14 +25,57 @@ app.get('/', (req, res) => {
     res.send('Hello from the home route!')
 });
 
-app.get('/weather', (req, res) => {
-    const query = req.query.searchQuery;
-    const weatherData = data.find(v => v.city_name === query);
-    res.send(weatherData.data.map(v => new WeatherForecast(v.datetime, v.high_temp, v.low_temp)))
-});
 
+
+app.get('/weather', getWeather);
+async function getWeather(req, res) {
+    const query = req.query.searchQuery;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${query}&key=${process.env.WEATHER_API_KEY}`;
+    try {
+        const weatherResponse = await axios.get(url);
+        const weatherArray = weatherResponse.data.data.map(weatherCity => new WeatherCity(weatherCity));
+        res.status(200).send(weatherArray);
+    } catch (error) {
+        console.log(`error message is: `, error);
+        response.status(500).send(`server error ${error}`);
+     }
+}
+
+app.get('/movies', getMovies);
+async function getMovies(req, res) {
+    const query = req.query.searchQuery;
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${process.env.MOVIE_API_KEY}`;
+    try {
+        const movieResponse = await axios.get(url);
+        const moviesArray = movieResponse.data.results.map(movieCity => new MovieCity(movieCity));
+        res.status(200).send(moviesArray);
+    } catch (error) {
+        console.log(`error message is: `, error);
+        response.status(500).send(`server error ${error}`);
+    }
+
+}
 
 //catch all endpoints:
 app.get('*', (req, res) => {
     res.status(404).send('Page Not Found')
 })
+
+class WeatherCity {
+    constructor(day) {
+        this.description = `Low of ${day.low_temp}, high of ${day.high_temp}} with ${day.weather.description}`;
+        this.date = day.valid_date;
+    }
+}
+
+class MovieCity {
+    constructor(obj) {
+        (this.title = obj.title),
+        (this.overview = obj.overview),
+        (this.average_votes = obj.vote_average),
+        (this.total_votes = obj.vote_count),
+        (this.image_url = 'https://image.tmdb.org/t/p/w500' + obj.poster_path),
+        (this.popularity = obj.popularity),
+        (this.released_on = obj.release_date);
+    }
+}
