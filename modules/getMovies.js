b29b280ead0eaf24bfb0fa4662ff5a580955f6fc
Mehtab Riar <mehtabriar228@hotmail.com>
2022-09-30 14:33:29 -0700

    added cache

--- modules/getMovies.js
+++ modules/getMovies.js
@@ -1,14 +1,30 @@
 'use strict';
 const axios = require('axios');
+const cache = require('./cache.js');
 
 
 async function getMovies(req, res) {
     const query = req.query.searchQuery;
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${process.env.MOVIE_API_KEY}`;
     try {
-        const movieResponse = await axios.get(url);
-        const moviesArray = movieResponse.data.results.map(movieCity => new MovieCity(movieCity));
-        res.status(200).send(moviesArray);
+        const key = query + 'movie'
+        //If the key exists in cache AND is valid then send THAT data from cache
+        if (cache[key] && (Date.now() - cache[key].timeStamp < 10000000000)) {
+            console.log('Cache was hit, movies are present')
+            res.status(200).send(cache[key].data)
+        } else {
+            const movieResponse = await axios.get(url);
+            const moviesArray = movieResponse.data.results.map(movieCity => new MovieCity(movieCity));
+            //save to cache
+            cache[key] = {
+                timeStamp: Date.now(),
+                data: moviesArray,
+            }
+            console.log('Cache is', cache)
+            res.status(200).send(moviesArray);
+
+        }
+
     } catch (error) {
         console.log(`error message is: `, error);
         response.status(500).send(`server error ${error}`);
--- modules/getWeather.js
+++ modules/getWeather.js
@@ -1,13 +1,25 @@
 'use strict';
 const axios = require('axios');
+const cache = require('./cache.js');
 
 async function getWeather(req, res) {
     const query = req.query.searchQuery;
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${query}&key=${process.env.WEATHER_API_KEY}`;
     try {
-        const weatherResponse = await axios.get(url);
-        const weatherArray = weatherResponse.data.data.map(weatherCity => new WeatherCity(weatherCity));
-        res.status(200).send(weatherArray);
+        const key = query + 'weather'
+        if (cache[key] && (Date.now() - cache[key].timeStamp < 860000000)) {
+            console.log('Cache was hit, weather is present')
+            res.status(200).send(cache[key].data)
+        } else {
+            const weatherResponse = await axios.get(url);
+            const weatherArray = weatherResponse.data.data.map(weatherCity => new WeatherCity(weatherCity));
+            cache[key] = {
+                timeStamp: Date.now(),
+                data: weatherArray,
+            }
+            console.log('Cache is', cache)
+            res.status(200).send(weatherArray);
+        }
     } catch (error) {
         console.log(`error message is: `, error);
         response.status(500).send(`server error ${error}`);
